a46d9c4783ee58c3e71eb420f452a2af023e8f00
qiaoyk <qiaoyk@geovis.com.cn>
2024-09-26 17:24:33 +0800

    first commit

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
 
     packages=find_packages(),
     include_package_data=True,
-    platforms="any",
-    install_requires = ["requests"]
+    platforms="any"
+    # install_requires = ["requests"]
 )
